b8d5f00c1bfbf6da494e1e8d09bbfead5e2155e5
shaikibrahim6030 <shaikibrahim6030@gmail.com>
2026-05-12 10:35:00 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import pickle
+import pandas as pd
+import numpy as np
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+# Load the trained model
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Get form data
+    hrs_Studied = float(request.form["hrs_Studied"])
+    prev_score = float(request.form["prev_score"])
+    sleep = int(request.form["sleep"])
+    sample_practiced = int(request.form["sample_practiced"])
+    extra_Curr = 1 if request.form["extra_Curr"] == "Yes" else 0
+
+    # Prepare features for prediction
+   
+    feature_names = ["hrs_Studied", "prev_score", "sleep", "sample_practiced", "extra_Curr"]
+    features = pd.DataFrame([[hrs_Studied, prev_score, sleep, sample_practiced, extra_Curr]], columns=feature_names)
+
+    # Predict charges
+    prediction = model.predict(features)
+    #  Format to float and 2 decimal places
+    formatted_prediction = f"The predicted value is ${round(float(prediction[0]), 2)}"
+
+
+    return render_template("result.html", prediction=formatted_prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
---
+++ train.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score, mean_squared_error
+
+
+
+
+
+# Load dataset (Example: Insurance Charges Dataset. Remove first column index)
+df = pd.read_csv("Student_Performance.csv")
+df["extra_Curr"] = df["extra_Curr"].map({"No": 0, "Yes": 1})
+
+
+
+
+
+# Features (X) and Target (y)
+X = df[["hrs_Studied", "prev_score",  "sleep", "sample_practiced", "extra_Curr"]]
+y = df["Result"]
+
+# Split data into training and testing sets (80% train, 20% test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Train Model
+model = LinearRegression()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+r2 = r2_score(y_test, predictions)
+print(f"Model Trained with R2 Score: {r2}")
+
+
+# Save model to a .pkl file
+with open("model.pkl", "wb") as file:
+    pickle.dump(model, file)
+
+
+
+print("Model trained and saved as model.pkl!")
